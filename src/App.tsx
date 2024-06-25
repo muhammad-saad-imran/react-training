eb22170ed367c4b2245520d96cd057b7727ed1ad
muhammad-saad-imran <muhammad.saad02@arbisoft.com>
2024-06-25 18:55:51 +0500

    ui code added

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   // const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='w-screen h-screen'>
       {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +31,7 @@ function App() {
       </p> */}
 
       <Login/>
-    </>
+    </div>
   )
 }
 
--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -3,7 +3,16 @@ import React from "react";
 type Props = {};
 
 const Login = (props: Props) => {
-  return <div>Login</div>;
+  return (
+    <div className="w-full h-full flex flex-col justify-center items-center">
+      <div className="border border-black p-4 rounded-md flex flex-col justify-center items-center ">
+        <p>Email</p>
+        <input type="email" />
+        <p>Password</p>
+        <input type="password" />
+      </div>
+    </div>
+  );
 };
 
 export default Login;
