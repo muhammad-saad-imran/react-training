2ab906fa5b6b8b3c702674c11e866af0a447ffcb
muhammad-saad-imran <muhammad.saad02@arbisoft.com>
2024-06-26 18:28:55 +0500

    Nextjs project init and RTK setup

---
+++ adaptive-insurance/src/lib/api/apiSlice.ts
@@ -0,0 +1,16 @@
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { apiBaseUrl } from './config'
+
+// Define our single API slice object
+export const apiSlice = createApi({
+  // The cache reducer expects to be added at `state.api` (already default - this is optional)
+  reducerPath: 'api',
+  // All of our requests will have URLs starting with '/fakeApi'
+  baseQuery: fetchBaseQuery({ baseUrl: apiBaseUrl }),
+  // The "endpoints" represent operations and requests for this server
+  endpoints: builder => ({})
+})
+
+// Export the auto-generated hook for the `getPosts` query endpoint
+export const { } = apiSlice
